330d321a6d7a115c1ab9d29ef190458626b5c30d
Wanderx13 <pupetfuntasy43@gmail.com>
2024-11-19 20:55:14 -0300

    inicio

---
+++ src/scripts/main.js
@@ -0,0 +1,33 @@
+
+
+
+const aniversario = new Date('Apr 13, 2025 15:00:00')
+const timeStampAniversario = aniversario.getTime()
+
+const contador = setInterval(function() {
+    const agora = new Date()
+    const timeStampAgora = agora.getTime()  
+
+    const distanciaAteAniversário = timeStampAniversario - timeStampAgora
+
+    const diasEMms = 1000 * 60 * 60 * 24
+    const horasEMms = 1000 * 60 * 60
+    const minutosEMms = 1000 * 60
+
+    const diasAteAniversario = Math.floor(distanciaAteAniversário / diasEMms)
+    const horasAteAniversario = Math.floor((distanciaAteAniversário % diasEMms) / horasEMms)
+    const minutosAteAniversario = Math.floor((distanciaAteAniversário % horasEMms) / minutosEMms)
+    const segundosAteAniversario = Math.floor((distanciaAteAniversário % minutosEMms) / 1000)
+
+    document.getElementById('data').innerHTML = `|
+    ${diasAteAniversario} - Dias |
+    ${horasAteAniversario} - Horas |
+    ${minutosAteAniversario} - Minutos |
+    ${segundosAteAniversario} - Segundos |`
+
+    if(timeStampAniversario <= timeStampAgora) {
+        document.getElementById('data').innerHTML = 'É SEU ANIVERSÁRIO, PARABÉNS!'
+        document.querySelector('.text').innerHTML = ' '
+        document.querySelector('.title').innerHTML = ' '
+    }
+}, 1000)
